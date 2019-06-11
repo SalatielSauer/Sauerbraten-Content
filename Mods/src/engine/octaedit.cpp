784cc2c5701c18b865734834622021aa9a5ee2d5
Salatiel <tiel_chaves@hotmail.com>
2019-06-11 15:46:41 -0300

    new selection commands

--- Mods/src/engine/octaedit.cpp
+++ Mods/src/engine/octaedit.cpp
@@ -1,4 +1,4 @@
-//Barbecue, allow edit in view, line 149
+//S: Allow Edit out of view
 VARP(editinview, 0, 1, 1);
 
 bool noedit(bool view, bool msg)
@@ -11,27 +11,48 @@ bool noedit(bool view, bool msg)
     o.add(s);
     r = float(max(s.x, max(s.y, s.z)));
     bool viewable = (isvisiblesphere(r, o) != VFC_NOT_VISIBLE);
-	
-	//Barbecue, allow edit in view
-	//if(!viewable && msg) conoutf(CON_ERROR, "selection not in view");
-	//return !viewable;
+
+    //S: Allow Edit out of view
+    /*if(!viewable && msg) conoutf(CON_ERROR, "selection not in view");
+    return !viewable;*/
     if(!viewable && editinview && msg) {conoutf(CON_ERROR, "selection not in view");}
-	if (editinview){
-    		return !viewable;
-	}
-	else
-	{
-		return false;	
-	}
+    if (editinview){return !viewable;} else {return false;}
 }
 
-//Barbecue, pushface
-//arg1: -1 || 1 (add or subtract arg2)
-//arg2: 0 = X || 1 = Y || 2 = Z
-void pushface(int *dir, int *pface)
+//Selection commands expansion, by Salatiel.
+
+//S: Get selection position, based on getcampos
+ICOMMAND(getselpos, "", (),
+{
+    defformatstring(pos)("%s %s %s", floatstr(sel.o.x), floatstr(sel.o.y), floatstr(sel.o.z));
+    result(pos);
+});
+
+/*
+S: Move selection to given position
+/setselpos x y z
+*/
+void setselpos(int *posx, int *posy, int *posz)
+{
+     sel.o.x = *posx - *posx % 16;
+     sel.o.y = *posy - *posy % 16;
+     sel.o.z = *posz - *posz % 16;
+}
+COMMAND(setselpos, "iii");
+
+/*
+S: Move selection to given direction (arg1: add/subtract | arg2: x y z)
+*/
+void movesel(int *dir, int *pface)
 {
     if(noedit(moving!=0)) return;
+
+    if((*pface > 2) || (*pface < 0))
+    {
+        conoutf(CON_ERROR, "valid range for movesel is 0..2");
+        return;
+    };
     int s = *dir;
     sel.o[*pface] += s*sel.grid;
 }
-COMMAND(pushface, "ii"); //
+COMMAND(movesel, "ii");
